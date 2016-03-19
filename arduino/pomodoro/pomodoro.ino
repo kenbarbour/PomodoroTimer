76234bb781817ba58e30e492ab65cd38f6ada05b
Ken Barbour <kenbarbour@gmail.com>
2016-03-19 10:43:18 -0400

    program works the way it was intended, but... there are some poor practices being employed:  - global variables  - completely open implementation (need to separate interfaces)  - timers need separate values for debugging and actual build I removed the Button class because it was no longer being used

--- arduino/pomodoro/pomodoro.ino
+++ arduino/pomodoro/pomodoro.ino
@@ -1,14 +1,11 @@
-#include <SoftwareSerial.h>
-#include "Button.h"
-
 /*
  Pomodoro timer
  
  Organizes time spent working and break time into sprints and breaks.
- Press the START_BUTTON to start the sprint timer.  The first LED should
+ Press the button to start the sprint timer.  The first LED should
  flash, letting you know the timer has started. Complete a sprint by
- working for a full WORK_TIME and pressing the START_BUTTON once the first
- LED starts blinking again.  After a sprint,
+ working for a full SPRINT_TIME and pressing the button once the first
+ LED starts blinking again.
  
  Components:
   * Green LED (x4)
@@ -17,81 +14,93 @@
   * 220 ohm Resistor (x5)
   
 */
+#define DEBUG_POMODORO 1
+#ifdef DEBUG_POMODORO
+#include <SoftwareSerial.h>
+#endif
 
-const int START_BUTTON_PIN = 2;
+const int BUTTON_PIN = 2;
 int LED_PINS[] = {9,6,5,3,7}; // Array of LED pins (TOTAL_SPRINTS + 1)
 
 //-- Timer configuration
-// number of sprints before a big break
-const int TOTAL_SPRINTS = 4;
-// time in milliseconds of a work sprint
-const int SPRINT_TIME = 25000;
-// time in seconds of a break
-const int BREAK_TIME = 25000;
-// time in seconds of a big reward break
-const int BIG_BREAK_TIME = 25000;
+const int TOTAL_SPRINTS = 4;        // number of sprints before a big break
+const int SPRINT_TIME = 25*60000;   // millisecs of a work sprint
+const int BREAK_TIME = 5*60000;     // millisecs of a break
+const int BIG_BREAK_TIME = 30*60000;// millisecs of a big reward break
 
-const int FLASH_DELAY = 100;
-const int FLASH_DELAY_QUICK = 25;
+const int FLASH_DELAY = 100;        // millisec delay b/w LED toggles during flashing
+const int FLASH_DELAY_QUICK = 50;   // millisec delay b/w LED toggles during quick flashing
+const int DEBOUNCE_DELAY = 50;      // min millisec delay b/w valid button state changes
 
-const int STATUS_BEFORE = 0; //  Before a sprint
-const int STATUS_SPRINT = 1; // During a sprint
-const int STATUS_AFTER = 2; // After a sprint
-const int STATUS_BREAK = 3; // During a break
-const int STATUS_BIG_BREAK = 4; // During a big break
+//-- Pomodoro States
+const int STATUS_BEFORE = 0;        // Before a sprint
+const int STATUS_SPRINT = 1;        // During a sprint
+const int STATUS_AFTER = 2;         // After a sprint
+const int STATUS_BREAK = 3;         // During a break
+const int STATUS_BIG_BREAK = 4;     // During a big break
 
-const int DEBUG = 1;
+// Declare Globals
+int currentSprint = 0;              // current sprint (zero based)
+int currentStatus = STATUS_BEFORE;  // current pomodoro state
+unsigned long sprintStartTime = 0;  // time (millisecs) current sprint/break started
+unsigned long lastDebounceTime = 0; // time (millisecs) button state last changed
+int buttonState = LOW;              // current state of button
+int lastButtonState = LOW;          // state of button last time it was checked
+unsigned long lastFlashTime = 0;    // time (millisecs) flashing LEDs were last toggled
+unsigned long lastFlashState = LOW; // pin state of flashing LEDs when they were last toggled
 
-// Declare vars
-int currentSprint = 0; // current sprint (zero based)
-int currentStatus = STATUS_BEFORE;
-unsigned long sprintStartTime = 0;
-Button* startButton;
+// function prototypes
+void updateSprintLEDs();
+void handleButton();
+void handleState();
+void debug(char*);
 
 void setup() {
   
+  #ifdef DEBUG_POMODORO
   Serial.begin(9600);
-  Serial.print("Initializing pins...\n");
+  #endif
+  debug("Init");
   // initialize pins
   for (int i = 0; i <= TOTAL_SPRINTS; i++) {
     pinMode(LED_PINS[i], OUTPUT);
   }
-  startButton = new Button(START_BUTTON_PIN,Button::MODE_PULLDOWN);
+  pinMode(BUTTON_PIN, INPUT);
   
   // flash LEDS
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 5; i++) {
     for (int j = 0; j <= TOTAL_SPRINTS; j++)
       digitalWrite(LED_PINS[j],HIGH);
-    delay(FLASH_DELAY);
+    delay(FLASH_DELAY_QUICK);
     for (int j = 0; j <= TOTAL_SPRINTS; j++)
       digitalWrite(LED_PINS[j],LOW);
     delay(FLASH_DELAY);
   }
+  debug("...done\n");
 }
 
 void loop() {
-  if (startButton->uniquePress())
-    onStartPressed();
-    
-  if (currentStatus == STATUS_SPRINT &&
-    sprintStartTime + SPRINT_TIME >= millis())
-    onSprintComplete();
+  handleButton();
+  handleState();
 }
 
 void onStartPressed() {
-  Serial.print("Start pressed\n");
-  delay(1000);
+  debug("button!\n");
   switch (currentStatus) {
     case STATUS_BEFORE:
-      onStartSprint();
+      startSprint();
       break;    
     case STATUS_SPRINT:
+      debug("Restart\n");
+      restartSprint();
       break;    
     case STATUS_AFTER:
+      startBreak();
       break;    
     case STATUS_BREAK:
-      break;    
     case STATUS_BIG_BREAK:
+      debug("Skipping\n");
+      breakComplete();
       break;    
   }
 }
@@ -100,16 +109,12 @@ void onStartPressed() {
 /**
  Starts the currentSprint if currentStatus is STATUS_BEFORE
  */
-void onStartSprint() {
+void startSprint() {
   
-  Serial.print("Starting sprint\n");
+  debug("Starting sprint\n");
   
   if (currentStatus != STATUS_BEFORE)
     return;
-  
-  // Illuminate completed sprints
-  for (int i = 0; i < currentSprint; i++)
-    digitalWrite(LED_PINS[i], HIGH);
     
   // Flash current sprint
   for (int i = 0; i < 5; i++) {
@@ -119,40 +124,145 @@ void onStartSprint() {
     delay(FLASH_DELAY);
   }
   
-  currentStatus = STATUS_SPRINT;
+  setStatus(STATUS_SPRINT);
   sprintStartTime = millis();
+}
+
+void restartSprint() {
+  debug("Restarting sprint\n");
+  setStatus(STATUS_BEFORE);
 }
 
 /**
  Completes the currentSprint if currentStatus is STATUS_SPRINT by setting
  the currentStatus to STATUS_AFTER and incrementing the currentSprint
  */
-void onSprintComplete() {
-  
-  Serial.print("Sprint complete\n");
+void sprintComplete() {
+  debug("Sprint complete\n");
   if (currentStatus != STATUS_SPRINT)
     return;
   currentSprint++;
-  currentStatus = STATUS_AFTER;
+  setStatus(STATUS_AFTER);
+  updateSprintLEDs();
 }
 
 /**
  Starts the break after a sprint if currentStatus is STATUS_AFTER by
  resetting the sprintStartTime to now and currentStatus to STATUS_BREAK
  */
-void onStartBreak() {
+void startBreak() {
   if (currentStatus != STATUS_AFTER)
     return;
+    
+  digitalWrite(LED_PINS[TOTAL_SPRINTS],HIGH);
     
   sprintStartTime = millis();
   if (currentSprint >= TOTAL_SPRINTS)
     currentStatus = STATUS_BIG_BREAK;
   else
-    currentStatus = STATUS_BREAK;
+    setStatus(STATUS_BREAK);
 }
 
-void onBreakComplete() {
-  if (currentStatus != STATUS_BREAK)
+void breakComplete() {
+  if (currentStatus != STATUS_BREAK && currentStatus != STATUS_BIG_BREAK)
     return;
-  currentStatus = STATUS_BEFORE;
+  digitalWrite(LED_PINS[TOTAL_SPRINTS],LOW);
+  if (currentStatus == STATUS_BIG_BREAK) {
+    currentSprint = 0;
+    updateSprintLEDs();
+  }
+  setStatus(STATUS_BEFORE);
+}
+
+void setStatus(int newStatus) {
+  switch (newStatus) {
+    case STATUS_BEFORE:
+      debug("BEFORE");
+      break;
+    case STATUS_SPRINT:
+      debug("SPRINT");
+      break;
+    case STATUS_AFTER:
+      debug("AFTER");
+      break;
+    case STATUS_BREAK:
+      debug("BREAK");
+      break;
+  }
+  debug("\n");
+  currentStatus = newStatus;
+}
+
+/**
+ * Checks if button has been pressed and calls onStartPressed() if true
+ */
+void handleButton() {
+  int reading = digitalRead(BUTTON_PIN); // read button state
+  
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+    // reading is older than debounce delay, it is valid
+    if (reading != buttonState) {
+      buttonState = reading;
+      
+      if (buttonState == HIGH)
+        onStartPressed();
+    }
+  }
+  lastButtonState = reading;
+}
+
+/**
+ *  Handle any logic that occurs on a loop for each state including flashing LEDS,
+ *  calling other handlers (except button presses)
+ */
+void handleState() {
+  switch (currentStatus) {
+    default:
+    case STATUS_BEFORE: // waiting on user input; flash current sprint led    
+      if ((millis() - lastFlashTime) > FLASH_DELAY_QUICK) {
+        int currentSprintPin = LED_PINS[currentSprint];
+        lastFlashState = !lastFlashState;
+        digitalWrite(currentSprintPin, lastFlashState);
+        lastFlashTime = millis();
+      }
+      break;
+    case STATUS_SPRINT:
+      if ((millis() - sprintStartTime) > SPRINT_TIME) {
+        sprintComplete();
+      }
+    break;
+    case STATUS_AFTER:
+      if ((millis() - lastFlashTime) > FLASH_DELAY_QUICK) {
+        lastFlashState = !lastFlashState;
+        digitalWrite(LED_PINS[TOTAL_SPRINTS], lastFlashState);
+        lastFlashTime = millis();
+      }
+    break;
+    case STATUS_BREAK:
+      if ((millis() - sprintStartTime) > BREAK_TIME) {
+        breakComplete();
+      }
+    break;
+    case STATUS_BIG_BREAK:
+      if ((millis() - sprintStartTime) > BIG_BREAK_TIME) {
+        breakComplete();
+      }
+    break;
+  }
+}
+
+void updateSprintLEDs() {
+  for (int i = 0; i < TOTAL_SPRINTS; i++) {
+    int value = (i < currentSprint) ? HIGH : LOW;
+    digitalWrite(LED_PINS[i],value);
+  }
+}
+
+void debug(const char* msg) {
+  #ifdef DEBUG_POMODORO
+  Serial.print(msg);
+  #endif
 }
